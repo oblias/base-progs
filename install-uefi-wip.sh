9a7b33bdaa8a02c40388eaee5446f84b204b4611
oblias <oblias@gmail.com>
2021-07-07 17:42:23 -0500

    Updated the install script again.

--- install-uefi-wip.sh
+++ install-uefi-wip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
-hwclock --systohc
+hwclock --systohc --localtime
 sed -i 'en_US.UTF-8' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-# echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
+echo "KEYMAP=us" >> /etc/vconsole.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
@@ -15,9 +15,9 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-# I need to update the below install files to suit me
+# I need to update the below install files to suit for laptop or desktop
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
@@ -35,11 +35,11 @@ systemctl enable avahi-daemon
 systemctl enable fstrim.timer
 # systemctl enable libvirtd
 # systemctl enable firewalld
-# systemctl enable acpid
+systemctl enable acpid
 
 useradd -m chaser
 echo chaser:password | chpasswd
-usermod -aG libvirt ermanno
+# usermod -aG libvirt ermanno
 
 echo "chaser ALL=(ALL) ALL" >> /etc/sudoers.d/chaser
 
